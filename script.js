6b553b44b606791951d8a985106fead293424e8c
Kaium Al Limon <limon.3cs@gmail.com>
2024-09-21 05:56:13 +0600

    initial push

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-// Check the URL for status query parameter
-window.onload = function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const status = urlParams.get('status');
+window.onload = function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const status = urlParams.get("status");
 
-
-    if (status === 'failure') {
-        // Redirect to the failure page
-        window.location.href = 'failure.html';
-    } else if (status === 'success') {
-        // Redirect to the success page
-        window.location.href = 'success.html';
-    } else if (status === 'cancel') {
-        // Redirect to the cancelled page
-        window.location.href = 'cancelled.html';
-    }
+  if (status === "failure") {
+    window.location.href = "failure.html";
+  } else if (status === "success") {
+    window.location.href = "success.html";
+  } else if (status === "cancel") {
+    window.location.href = "cancelled.html";
+  }
 };
